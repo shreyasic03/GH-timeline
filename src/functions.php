aea5ccb85c06ba9fa14cbfa94ce0ed84f67c5136
Shreyasi Chowdhury <148044884+shreyasic03@users.noreply.github.com>
2025-07-02 20:40:12 +0530

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,53 @@
+<?php
 
+/**
+ * Generate a 6-digit numeric verification code.
+ */
+function generateVerificationCode(): string {
+    // TODO: Implement this function
+}
+
+/**
+ * Send a verification code to an email.
+ */
+function sendVerificationEmail(string $email, string $code): bool {
+    // TODO: Implement this function
+}
+
+/**
+ * Register an email by storing it in a file.
+ */
+function registerEmail(string $email): bool {
+  $file = __DIR__ . '/registered_emails.txt';
+    // TODO: Implement this function
+}
+
+/**
+ * Unsubscribe an email by removing it from the list.
+ */
+function unsubscribeEmail(string $email): bool {
+  $file = __DIR__ . '/registered_emails.txt';
+    // TODO: Implement this function
+}
+
+/**
+ * Fetch GitHub timeline.
+ */
+function fetchGitHubTimeline() {
+    // TODO: Implement this function
+}
+
+/**
+ * Format GitHub timeline data. Returns a valid HTML sting.
+ */
+function formatGitHubData(array $data): string {
+    // TODO: Implement this function
+}
+
+/**
+ * Send the formatted GitHub updates to registered emails.
+ */
+function sendGitHubUpdatesToSubscribers(): void {
+  $file = __DIR__ . '/registered_emails.txt';
+    // TODO: Implement this function
+}
