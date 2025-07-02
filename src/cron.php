aa5a8113a16eb0d48dc7f751aef729b84304875e
Shreyasi Chowdhury <148044884+shreyasic03@users.noreply.github.com>
2025-07-02 20:45:45 +0530

    Update cron.php

--- src/cron.php
+++ src/cron.php
@@ -1,4 +1,3 @@
 <?php
 require_once 'functions.php';
-// This script should send GitHub updates to registered emails every 5 minutes.
-// You need to implement this functionality.
+sendGitHubUpdatesToSubscribers();
